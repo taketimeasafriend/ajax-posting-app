ac79745c1b7671e645cdfe2bd1ef094e0c71474e
taketimeasafriend <taketimeasafriend@gmail.com>
2017-04-06 08:03:16 +0800

    Ajax delete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,7 +17,7 @@ class PostsController < ApplicationController
         @post = current_user.posts.find(params[:id]) # 只能删除自己的贴文
         @post.destroy
 
-        redirect_to posts_path
+        #render :js => "alert('ok');"
     end
 
 
